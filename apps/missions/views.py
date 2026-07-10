fb21c71614196d97c64f65004d5cc4ba2cc95c25
Nima <nimiologyy@gmail.com>
2026-07-10 18:51:55 +0330

    feat(missions): bulk assign missions and squad UX improvements

--- apps/missions/views.py
+++ apps/missions/views.py
@@ -6,6 +6,7 @@ from .serializers import MissionSerializer
 from apps.accounts.permissions import IsCoachOrReadOnly
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import status
 from django.db.models import Count
 class MissionViewSet(viewsets.ModelViewSet):
     queryset = Mission.objects.all()
@@ -31,11 +32,34 @@ class MissionViewSet(viewsets.ModelViewSet):
             Q(athlete=user) | Q(squad__athletes=user)
         ).distinct()
 
+    def create(self, request, *args, **kwargs):
+        # Support creating multiple missions for a list of athletes
+        data = request.data.copy() if hasattr(request.data, 'copy') else dict(request.data)
+        athletes = data.pop('athletes', None)
+        # If it was a QueryDict, pop might return a string or list. For JSON, it's a list.
+        if isinstance(athletes, str):
+            import json
+            try:
+                athletes = json.loads(athletes)
+            except:
+                athletes = [athletes]
+                
+        if athletes and isinstance(athletes, list):
+            created_missions = []
+            for athlete_id in athletes:
+                athlete_data = data.copy()
+                athlete_data['athlete'] = athlete_id
+                serializer = self.get_serializer(data=athlete_data)
+                serializer.is_valid(raise_exception=True)
+                self.perform_create(serializer)
+                created_missions.append(serializer.data)
+            return Response(created_missions, status=status.HTTP_201_CREATED)
+            
+        return super().create(request, *args, **kwargs)
+
     def perform_create(self, serializer):
         # Set the coach as the logged-in user
         serializer.save(coach=self.request.user)
-
-    
 
     @action(detail=False, methods=['get'])
     def summary(self, request):
